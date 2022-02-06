e5f26975cfdb04b7364fe5d59df45d569b427cc8
felipe2204sv <felipe2204sv@gmail.com>
2022-02-06 18:54:38 -0300

    Initial commit

---
+++ app/src/main/java/com/felipesvieira/notesapp/ListFragment.kt
@@ -0,0 +1,12 @@
+package com.felipesvieira.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ListFragment : Fragment() {
+
+}
